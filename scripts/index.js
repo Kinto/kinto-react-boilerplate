33ab586e71dce6337e54e19dd9bd3a2f22605a39
Mathieu Leplatre <mathieu@mozilla.com>
2015-08-11 12:46:43 +0200

    Specify collection name in store

--- scripts/index.js
+++ scripts/index.js
@@ -5,11 +5,19 @@ import App from "./components/App";
 import { Store } from "./store";
 import Kinto from "kinto";
 
-const server = "https://kinto.dev.mozaws.net/v1";
-const userpass = (window.location.hash.slice(1) || "public") + ":s3cr3t";
-const auth = "Basic " + btoa(userpass);
+// Take username from location hash (With default value):
+const user = window.location.hash = (window.location.hash.slice(1) || "public-demo");
+const userpass64 = btoa(user + ":s3cr3t");
 
+// Use Mozilla demo server with Basic authentication:
+const server = "https://kinto.dev.mozaws.net/v1";
+const auth = "Basic " + userpass64;
 const kinto = new Kinto({remote: server, headers: {Authorization: auth}});
-const store = new Store(kinto);
+
+const store = new Store(kinto, "items");
+
+// Make sure local data depend on current user.
+// Note: Kinto.js will have an option: https://github.com/Kinto/kinto.js/pull/111
+store.collection.db.dbname = userpass64 + store.collection.db.dbname;
 
 React.render(<App store={store}/>, document.getElementById("app"));
